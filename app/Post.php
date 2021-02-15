7c00d478a8d7a31eb84c805a834e3e9a6c94fe96
Jonathan <mcjhona069@gmail.com>
2021-02-15 11:51:49 -0600

    config inicial

---
+++ app/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use Sluggable;
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
+}
