786261c25faa473fbae1c8a98b71a963536a15fd
Jonathan <mcjhona069@gmail.com>
2021-02-15 13:50:50 -0600

    Creación de tablas, entidades y datos de prueba

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        App\User::create([
+            'name' => 'Jonathan Pérez',
+            'email' => 'J@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        factory(App\Post::class, 24)->create();
+
     }
 }
